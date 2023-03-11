f569e05f41d08b6e85279d22786439a00725a12f
Mohd Naushad Ahmed <naushad.dln@gmail.com>
2023-03-11 18:08:09 +0530

    Updated

--- src/app/tracking/multiple-shipment/multiple-shipment.component.ts
+++ src/app/tracking/multiple-shipment/multiple-shipment.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Shipment } from 'src/common/app-globals';
+import { HttpsvcService } from 'src/common/httpsvc.service';
 
 @Component({
   selector: 'app-multiple-shipment',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MultipleShipmentComponent implements OnInit {
 
-  constructor() { }
+  multipleShipmentTrackingForm?: FormGroup;
+  whichVendor: string = "";
+  shipments?: Array<Shipment>;
+  
+  constructor(private http: HttpsvcService, private fb: FormBuilder) { 
+    this.multipleShipmentTrackingForm = this.fb.group({
+      shipmentNo:'',
+      altRefNo: '',
+      vendor: 'self'
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  onVendorSelect(what: string) {
+    this.whichVendor = what;
   }
 
 }
